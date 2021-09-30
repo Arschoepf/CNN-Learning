880b5952321c5705aa1c9304c3330cd63ec00654
Alex Schoepf <arschoepf@comcast.net>
2021-09-30 00:00:33 +0000

    Reformatted code to make it more readable

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-class Main {
-  public static void main(String[] args) {
-    double [][][] input = 
+public class Main {
+  public static void main(String[] args) 
+  {
+    new Main().run();
+  }
+
+  public void run()
+  {
+    
+    double[][][] input = 
     {
       { {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0} },
       { {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0} },
@@ -12,13 +19,16 @@ class Main {
       { {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0} }
     };
 
-    double [][][] filter = 
+    double[][][][] filter = 
     {
-      { {-1}, {0}, {1} },
-      { {-1}, {0}, {1} },
-      { {-1}, {0}, {1} }
+      {
+        { {-1}, {-1}, {-1} },
+        { {0}, {0}, {0} },
+        { {1}, {1}, {1} }
+      }
     };
 
+    System.out.println("Input Matrix");
     for(int d = 0; d < input[0][0].length; d++)
     {
       for(int h = 0; h < input[0].length; h++)
@@ -32,31 +42,71 @@ class Main {
       System.out.println();
     }
 
-    for(int d = 0; d < input[0][0].length; d++)
+    System.out.println("Filter Matrices");
+    for(int f = 0; f < filter[0][0][0].length; f++)
     {
-      for(int h = 1; h < input[0].length -1; h++)
+      for(int d = 0; d < filter[0][0].length; d++)
       {
-        for(int w = 1; w < input.length -1; w++)
+        for(int h = 0; h < filter[0].length; h++)
         {
-          double sum = 0;
-          for(int i = 0; i < filter[0][0].length; i++)
+          for(int w = 0; w < filter.length; w++)
           {
-            for(int j = 0; j < filter[0].length; j++)
-            {
-              for(int k = 0; k < filter.length; k++)
-              {
-                sum += (input[w+k-1][h+j-1][d] * filter[k][j][i]);
-                //System.out.print (input[w][h][d] * filter[k][j][i] + " ");
-              }
-              //System.out.println();
-            }
-            //System.out.println();
+            System.out.print(filter[w][h][d][f] + " ");
           }
-          System.out.print("   " + sum + "   ");
+        }
+        System.out.println();
+      }
+      System.out.println();
+    }
+
+    double[][][] convoluted = Convolution(input, filter);
+    
+    System.out.println("Output Matrix");
+    for(int d = 0; d < convoluted[0][0].length; d++)
+    {
+      for(int h = 0; h < convoluted[0].length; h++)
+      {
+        for(int w = 0; w < convoluted.length; w++)
+        {
+          System.out.print(convoluted[w][h][d] + " ");
         }
         System.out.println();
       }
       System.out.println();
     }
   }
+
+  public double[][][] Convolution(double[][][] input, double[][][][] kernels) {
+    int k = kernels[0].length/2;  //Kernel indexing from -1 to 1
+    int F = kernels.length;  //Number of filters
+    int W = input.length; //Width of input image
+    int H = input[0].length;  //Height of input image
+    int D = kernels.length; //Depth of images and kernels
+
+    if(D != input [0][0].length) throw new IllegalArgumentException("Input and kernel depths don't match"); //Throw error if depths don't match
+
+    double[][][] convoluted = new double[W-2*k][H-2*k][D];  //create variable for convoluted matrix
+
+    for(int f = 0; f < F; f++)  //Input and kernel filters
+    {
+      for(int h = k; h < H - k; h++) //Input height
+      {
+        for(int w = k; w < W - k; w++)  //Input width
+        {
+          convoluted[w - k][h - k][f] = 0;
+          for(int j = -k; j <= k; j++) //Kernel height
+          {
+            for(int i = -k; i <= k; i++)  //Kernel width
+            {
+              for(int d = 0; d < D; d++)  //Kernel depth
+              {
+                convoluted[w - k][h - k][f] += (input[w + i][h + j][d] * kernels[f][i + k][j + k][d]);
+              }
+            }
+          }
+        }
+      }
+    }
+    return convoluted;
+  } 
 }
